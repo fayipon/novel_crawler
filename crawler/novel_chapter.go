4af61fc4f148bb12d2671829606f5c86d20e5d88
Fayi <fayi.pon@gmail.com>
2023-12-25 10:50:36 +0800

    Update novel_chapter.go

--- crawler/novel_chapter.go
+++ crawler/novel_chapter.go
@@ -3,6 +3,7 @@ package main
 import (
     "fmt"
     "log"
+    "os"
     "github.com/PuerkitoBio/goquery"
     "net/http"
 )
@@ -29,10 +30,37 @@ func main() {
         log.Fatal(err)
     }
 
+    // 获取页面标题
+    title := doc.Find("title").Text()
+
+    // 指定目录路径
+    dirPath := "./novel" // 例如，将文件夹保存在当前工作目录下的 "novel" 目录中
+
+    // 创建目录（如果不存在）
+    err = os.MkdirAll(dirPath, os.ModePerm)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    // 创建txt文件并打开以供写入
+    filePath := fmt.Sprintf("%s/%s.txt", dirPath, title)
+    file, err := os.Create(filePath)
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer file.Close()
+
     // 选择所有带有<p style="color: black;">的元素
-    doc.Find("p").Each(func(index int, element *goquery.Selection) {
-        // 提取文本内容并打印
+    doc.Find("p[style='color: black;']").Each(func(index int, element *goquery.Selection) {
+        // 提取文本内容
         text := element.Text()
-        fmt.Println("文本内容:", text)
+        
+        // 写入文本内容到txt文件中
+        _, err := file.WriteString(text + "\n")
+        if err != nil {
+            log.Fatal(err)
+        }
     })
+
+    fmt.Printf("文本已保存到 %s\n", filePath)
 }
