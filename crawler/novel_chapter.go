6249be3622799104de456ccad3c19357e0ea6f73
Fayi <fayi.pon@gmail.com>
2023-12-25 10:29:46 +0800

    Update novel_chapter.go

--- crawler/novel_chapter.go
+++ crawler/novel_chapter.go
@@ -4,6 +4,7 @@ import (
     "fmt"
     "log"
     "github.com/PuerkitoBio/goquery"
+    "net/http"
 )
 
 func main() {
@@ -11,7 +12,19 @@ func main() {
     url := "https://sto520.com/book/27711/"
 
     // 发起HTTP GET请求
-    doc, err := goquery.NewDocument(url)
+    res, err := http.Get(url)
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer res.Body.Close()
+
+    // 检查响应状态码
+    if res.StatusCode != http.StatusOK {
+        log.Fatalf("HTTP request failed with status code: %d", res.StatusCode)
+    }
+
+    // 使用goquery选择器来解析页面元素
+    doc, err := goquery.NewDocumentFromReader(res.Body)
     if err != nil {
         log.Fatal(err)
     }
@@ -19,7 +32,7 @@ func main() {
     // 使用goquery选择器来解析页面元素
     // 这里以获取标题为例
     title := doc.Find("h1").Text()
-    
+
     // 输出标题
     fmt.Println("标题:", title)
 }
