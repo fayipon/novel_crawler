af65a751a978eef86a1bd5c10f7c7d0491535426
Fayi <fayi.pon@gmail.com>
2023-12-25 10:23:45 +0800

    Create novel_chapter.go

---
+++ crawler/novel_chapter.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+func main() {
+	// 要抓取的網址
+	url := "https://sto520.com/book/27711/"
+
+	// 發送 HTTP GET 請求
+	response, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer response.Body.Close()
+
+	// 使用 goquery 解析 HTML
+	doc, err := goquery.NewDocumentFromReader(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// 找到所有的 <dd> 元素並取出文字
+	doc.Find("dd").Each(func(index int, element *goquery.Selection) {
+		text := element.Text()
+		fmt.Printf("Text in <dd> %d: %s\n", index+1, text)
+	})
+}
